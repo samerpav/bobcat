0b18a6e3100e364c61bd15d712f83158f8ce78fe
Sam Vesurai <sam94089@gmail.com>
2011-09-18 22:25:27 -0700

    cleaned up code

--- parsers/pts.js
+++ parsers/pts.js
@@ -4,7 +4,6 @@ var PTSParser = (function() {
     
     var undef;
     
-    // defined once to reduce number of empty functions
     var __empty_func = function(){};
   
     var start = config.start || __empty_func;
@@ -19,96 +18,21 @@ var PTSParser = (function() {
     var numTotalPoints = 0;
     var progress = 0;
         
-    // keep track if onprogress event handler was called to 
-    // handle Chrome/WebKit vs. Minefield differences.
-    //
-    // Minefield will call onprogress zero or many times
-    // Chrome/WebKit will call onprogress one or many times
     var onProgressCalled = false;
     var FR = null;
     
-    /**
-      Returns the version of this parser.
-      @name PTSParser#version
-      @returns {String} parser version.
-    */
-    this.__defineGetter__("version", function(){
-      return VERSION;
-    });
+    this.__defineGetter__("version", function(){ return VERSION; });
+    this.__defineGetter__("numParsedPoints", function(){ return numParsedPoints; });
+    this.__defineGetter__("numTotalPoints", function(){ return numTotalPoints; });
+    this.__defineGetter__("progress", function(){ return progress; });
+    this.__defineGetter__("fileSize", function(){ return fileSizeInBytes; });
     
-    /**
-      Get the number of parsed points so far.
-      @name PTSParser#numParsedPoints
-      @returns {Number} number of points parsed.
-    */
-    this.__defineGetter__("numParsedPoints", function(){
-      return numParsedPoints;
-    });
+    this.stop = function(){ if(FR){ FR.abort(); } };
     
-    /**
-      Get the total number of points in the point cloud.
-      @name PTSParser#numTotalPoints
-      @returns {Number} number of points in the point cloud.
-    */
-    this.__defineGetter__("numTotalPoints", function(){
-      return numTotalPoints;
-    });
-    
-    /**
-      Returns the progress of downloading the point cloud between zero and one or
-      -1 if the progress is unknown.
-      @name PTSParser#progress
-      @returns {Number|-1}
-    */
-    this.__defineGetter__("progress", function(){
-      return progress;
-    });
-    
-    /**
-      Returns the file size of the resource in bytes.
-      @name PTSParser#fileSize
-      @returns {Number} size of resource in bytes.
-    */
-    this.__defineGetter__("fileSize", function(){
-      return fileSizeInBytes;
-    });
-    
-    /**
-      Stop downloading and parsing the associated point cloud.
-    */
-    this.stop = function(){
-      if(FR){
-        FR.abort();
-      }
-    };
-    
-    /**
-      @param {Object} file
-    */
-    this.load = function(file){
+    // parser's load function
+    this.load = function(file) {
 
-      //FR.onprogress = function(evt) {
-      //   console.log(evt.loaded);
-      //}
-
-      //FR.onload = function(evt){
-      //
-      //  //var data = FR.result;
-      //  
-      //  console.log(evt.loaded);	
-
-      //  //var test = new Uint8Array(data);
-      //  //FR.parseChunk(data);
-
-      //  numTotalPoints = numParsedPoints;
-      //  
-      //  // Indicate parsing is done. ranges from 0 to 1
-      //  progress = 1;
-      //  
-      //  end(FR.parser);
-      //}
-      
-      var chunkSize = 1024*1024*1,                  // it looked like 1M chunk is fastest and uses least memory
+      var chunkSize = 1024*1024*1, // it looked like 1M chunk is fastest and uses least memory
       chunks = Math.ceil(file.size / chunkSize),
       _chunk = 0;
 
@@ -173,7 +97,6 @@ var PTSParser = (function() {
         };
 
         FR.parseChunk = function(chunk){
-		
 
             // this occurs over network connections, but not locally.
             if(chunk !== ""){
@@ -187,7 +110,6 @@ var PTSParser = (function() {
               // trim leading spaces
               chunk = chunk.replace(/^\s+/,"");
 
-              // sam
               // find out how many columns per line
               //var firstline = chunk.split(/\n/, 1);
               //var numActualColumns = firstline[0].split(" ").length;
@@ -199,21 +121,17 @@ var PTSParser = (function() {
               var numVerts = chunk.length/numValuesPerLine;
               numParsedPoints += numVerts;
               
-	        // we use interleaved array to reduce number of Draw calls from 2 to 1
-		var elementSize = 3 * Float32Array.BYTES_PER_ELEMENT + 4 * Uint8Array.BYTES_PER_ELEMENT;
-		var buf = new ArrayBuffer(numVerts * elementSize);
-		var coords = new Float32Array(buf);
-		var colors = new Uint8Array(buf, 3 * Float32Array.BYTES_PER_ELEMENT);
+	      // we use interleaved array to reduce number of Draw calls from 2 to 1
+	      var elementSize = 3 * Float32Array.BYTES_PER_ELEMENT + 4 * Uint8Array.BYTES_PER_ELEMENT;
+	      var buf = new ArrayBuffer(numVerts * elementSize);
+	      var coords = new Float32Array(buf);
+	      var colors = new Uint8Array(buf, 3 * Float32Array.BYTES_PER_ELEMENT);
+	      //var intensity;
 
-              //var verts = new Float32Array(numVerts * 3);
-              //var cols =  new Float32Array(numVerts * 3);     // color doesn't need Float32
-	      //var cols = new Uint8Array(numVerts * 3);
-              //var intensity;
+	      var coordOffset = elementSize / Float32Array.BYTES_PER_ELEMENT;
+	      var colorOffset = elementSize / Uint8Array.BYTES_PER_ELEMENT;
 
-		var coordOffset = elementSize / Float32Array.BYTES_PER_ELEMENT;
-		var colorOffset = elementSize / Uint8Array.BYTES_PER_ELEMENT;
-
-              // x y z  intensity r g b
+              // x y z intensity r g b
               for(var i=0, j=0; i<chunk.length; i += numValuesPerLine, j++){
 
 		coords[0+j*coordOffset] = parseFloat(chunk[i]); 
@@ -224,14 +142,6 @@ var PTSParser = (function() {
 		colors[1+j*colorOffset] = parseInt(chunk[i+5]);
 		colors[2+j*colorOffset] = parseInt(chunk[i+6]);
 		colors[3+j*colorOffset] = 255;
-
-                //verts[j]   = parseFloat(chunk[i]);
-                //verts[j+1] = parseFloat(chunk[i+1]);
-                //verts[j+2] = parseFloat(chunk[i+2]);
-
-		//cols[j]   = parseInt(chunk[i+4]);
-		//cols[j+1] = parseInt(chunk[i+5]);
-		//cols[j+2] = parseInt(chunk[i+6]);
 
                 //switch (numActualColumns) {
 		//	case 7: // color
@@ -257,27 +167,20 @@ var PTSParser = (function() {
 		//		cols[j+1] = 128;
 		//		cols[j+2] = 0;
                 //}
-              }
+              } // for
                     
               var attributes = {};
 
-              if (coords) {attributes["ps_Vertex"] = buf;}
-
-              //if (verts) {attributes["ps_Vertex"] = verts;}
-              //if(cols){attributes["ps_Color"] = cols;}
-              
-	      var xhr = new XMLHttpRequest();
-	      xhr.open('POST', '/form', true);
-	      xhr.onload = function(e) {  };
-	      xhr.send(buf);
+              if (coords) { attributes["ps_Vertex"] = buf; }
 
               parse(FR.parser, attributes);
             }
         };
 
         FR.readAsBinaryString(blobSlice.call(file, start, end));
-        //FR.readAsText(blobSlice.call(file, start, end));       -- this uses more memory than readAsBinaryString
 
+	// this uses more memory than readAsBinaryString
+        //FR.readAsText(blobSlice.call(file, start, end));       
       }
 
       loadNext();
